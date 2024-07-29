ab2a2b9c7835975a5a2f5f5ba2809e02b49893f6
Tri Nguyen <tri.nguyen@linecorp.com>
2024-07-29 22:55:33 +0700

    Update script

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,9 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-  },
+  base: '/dragon-prime-gym/',
   server: {
     port: 3000,
-    // Make sure Vite is serving JavaScript modules correctly
-    mimeTypes: {
-      '.js': 'application/javascript',
-    },
   },
   resolve: {
     alias: [
