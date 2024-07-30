07b3e0c55018fef5f63481cde2f63f4e91914cf4
tringuyen093 <tringuyen093@users.noreply.github.com>
2024-07-30 08:04:09 +0000

    deploy: 6a9526c0bf39a1e8d730b7e3d71e48388261199c

---
+++ assets/index.2bksH_Ed.js
@@ -0,0 +1,8 @@
+import{d as o,j as e}from"./index.DPHhV1wU.js";const r="/assets/1.dJQo2nG2.jpg",t=o.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  background-image: url(${r});
+  background-size: cover;
+  background-position: center;
+`,n=()=>e.jsx(t,{}),a=()=>e.jsx(n,{});export{a as default};
