828df45fecce88b50ddd541706e285ae130419b0
tringuyen093 <tringuyen093@users.noreply.github.com>
2024-07-30 07:16:10 +0000

    deploy: 475d136c30558959fe5d0bbc6647b26dc1c4ff20

---
+++ assets/index.W8tgW3Tu.js
@@ -0,0 +1,8 @@
+import{d as r,j as e}from"./index.Ito-6src.js";const o="/dragon-prime-gym/assets/1.dJQo2nG2.jpg",n=r.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  background-image: url(${o});
+  background-size: cover;
+  background-position: center;
+`,t=()=>e.jsx(n,{}),a=()=>e.jsx(t,{});export{a as default};
