66238c211abacae00823cdae2485edc27aeadf91
tringuyen093 <tringuyen093@users.noreply.github.com>
2024-07-31 09:19:22 +0000

    deploy: cb6be077957f40f3f19747aebcbc5b425d8e4499

--- assets/index.B9sTjrHh.js
+++
@@ -1,8 +0,0 @@
-import{d as o,j as e}from"./index.BI9K4-S0.js";const r="/assets/1.dJQo2nG2.jpg",t=o.div`
-  display: flex;
-  width: 100%;
-  height: 100%;
-  background-image: url(${r});
-  background-size: cover;
-  background-position: center;
-`,n=()=>e.jsx(t,{}),a=()=>e.jsx(n,{});export{a as default};
---
+++ assets/index.DjvE2-2v.js
@@ -0,0 +1,8 @@
+import{d as r,j as e}from"./index.NvKP4GHZ.js";const t="/assets/1.DfUneHpO.jpg",n=r.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  background-image: url(${t});
+  background-size: cover;
+  background-position: center;
+`,o=()=>e.jsx(n,{}),a=()=>e.jsx(o,{});export{a as default};
