b2dafbe91f7f55279ba4c0f91ef3717218dda281
tringuyen093 <tringuyen093@users.noreply.github.com>
2024-11-23 08:04:57 +0000

    deploy: 7c2f0952bd40000609a573a3ac0bfa205303dc8d

--- assets/index.Dri02h0_.js
+++
@@ -1,8 +0,0 @@
-import{d as r,j as e}from"./index.PGZ77osR.js";const t="/assets/1.DfUneHpO.jpg",n=r.div`
-  display: flex;
-  width: 100%;
-  height: 100%;
-  background-image: url(${t});
-  background-size: cover;
-  background-position: center;
-`,o=()=>e.jsx(n,{}),a=()=>e.jsx(o,{});export{a as default};
---
+++ assets/powerliftingBar.Cj1fMLe4.js
@@ -0,0 +1,32 @@
+import{d as a,j as e}from"./index.CHvD_k2T.js";import{S as r,A as i,N as n,a as o,_ as p}from"./navigation.Cr4s99Mc.js";const l="/assets/1.Cpurn5vR.jpg",c="/assets/2.D2au5KMF.jpg",d="/assets/3.CfP5i3OM.jpg",u=a.div`
+  width: 100%;
+  height: 100%;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+
+  .swiper {
+    margin: 0;
+    height: 100%;
+
+    .swiper-slide {
+      height: 100%;
+      aspect-ratio: 9 / 16;
+
+      img {
+        object-fit: cover;
+      }
+    }
+  }
+
+  .swiper-button-prev,
+  .swiper-button-next {
+    color: var(--color-gray-500);
+  }
+
+  .lazyload-wrapper {
+    height: 100%;
+  }
+`,g=[l,c,d],h=()=>e.jsx(u,{children:e.jsx(r,{slidesPerView:1,spaceBetween:50,navigation:!0,loop:!0,autoplay:{delay:3e3,disableOnInteraction:!1},modules:[i,n],className:"suplement-swiper",children:g.map((t,s)=>e.jsx(o,{children:e.jsx(p,{children:e.jsx("img",{src:t,alt:t,loading:"lazy"})})},s))})}),j=()=>e.jsx(h,{});export{j as default};
