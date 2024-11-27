10686fad498f74cf7fcacd1f20b066a1f8441918
James / Tri Nguyen <tri.nguyen@linecorp.com>
2024-11-27 22:47:52 +0700

    revert examplee

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -3,12 +3,11 @@ import banner from '@/assets/images/equipments/1.jpg';
 
 const ContainerWrapper = styled.div`
   display: flex;
-  width: 90%;
-  height: 90%;
+  width: 100%;
+  height: 100%;
   background-image: url(${banner});
-  background-size: contain;
-  background-position: left bottom;
-  color: white;
+  background-size: cover;
+  background-position: center bottom;
 
   @media (max-width: 768px) {
     background-position: left bottom;
