7bd34c13f6a9250a5568f9480b2ae26636795707
tringuyen093 <tringuyen093@users.noreply.github.com>
2024-11-27 15:03:29 +0000

    deploy: 0faed57acd530beb55e8ce8602a82e710d4da380

--- assets/index.CYkoiquy.js
+++
@@ -1,8 +0,0 @@
-import{d as r,j as e}from"./index.CHvD_k2T.js";const t="/assets/1.D651p4TO.jpg",o=r.div`
-  display: flex;
-  width: 100%;
-  height: 100%;
-  background-image: url(${t});
-  background-size: cover;
-  background-position: center;
-`,s=()=>e.jsx(o,{}),a=()=>e.jsx(s,{});export{a as default};
